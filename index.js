b598c83d71fe6a86dd29d897dfdc9b37bcbc8d88
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-01-28 20:36:57 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,4 @@
+// freelancer array
+// calculate and display the average starting price
+// add new freelancers dynam,ically every few seconds
+// update the freelancer list and average price in real-time
